9036396b8f1ef38159c19326d89186ee60322e01
Valentin Pelloin <valentinpelloin@gmail.com>
2018-01-23 22:03:44 +0100

    rspec for unit testing + test coverage (normally this doesn't build)

--- Rakefile
+++ Rakefile
@@ -1,13 +1,16 @@
 require 'rspec/core/rake_task'
 require 'simplecov'
 
+# Default task: we run all tests
 task :default => :tests
 
+# Tests: we run spec (unit tests) and the coverage tests
 task :tests => [:spec, :coverage]
 
+# Spec: Unit tests
 RSpec::Core::RakeTask.new(:spec)
 
-desc "Run the test coverage"
+# Coverage tests
 task :coverage do
 	SimpleCov.start do
 		add_filter "/spec/"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,4 @@
-#require 'simplecov'
-#SimpleCov.start do
-#	add_filter '/spec/' # for rspec
-#end
-
-#require 'simplecov'
-
-#SimpleCov.start do 
-
-	# filter on spec folder: we don't want to calculate unit tests in test coverage
-#	add_filter "spec/*"
-#end
+# Here, we just have to list all .rb files that we want to run unit tests
+# all files needs a name_spec.rb with rspec unit test inside
 
 require_relative '../lib/awesome'
