50b61bd914b6d7aaa356bf65dcf175c24686f821
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2018-01-25 23:02:20 +0100

    fix unit test with new state

--- spec/Cell_spec.rb
+++ spec/Cell_spec.rb
@@ -3,34 +3,40 @@ require 'spec_helper'
 
 describe Cell do
 
-	cell = Cell.new 
+	cell = Cell.new
 
 	it "initialize cell" do
 		expect(cell.state).to eq Cell::CELL_EMPTY
 	end
 
-	it "change cell state to crossed" do
+	it "should display the cell" do
+			expect(cell.to_s).to eq "░"
+
+			cell.state = Cell::CELL_WHITE
+			expect(cell.to_s).to eq "W"
+
+			cell.state = Cell::CELL_CROSSED
+			expect(cell.to_s).to eq "X"
+
+			cell.state = Cell::CELL_BLACK
+			expect(cell.to_s).to eq "█"
+	end
+
+
+	it "test all cell states" do
+		cell.state = Cell::CELL_BLACK
+		expect(cell.state).to eq Cell::CELL_BLACK
+
+		cell.state = Cell::CELL_WHITE
+		expect(cell.state).to eq Cell::CELL_WHITE
+
 		cell.state = Cell::CELL_CROSSED
 		expect(cell.state).to eq Cell::CELL_CROSSED
 	end
 
-	it "change cell state to full" do
-		cell.state = Cell::CELL_FULL
-		expect(cell.state).to eq Cell::CELL_FULL
-	end
-
 	it "should raise" do
     	expect{cell.state=3}.to raise_error(ArgumentError)
-	end	
+	end
 
-	it "should display the cell" do
-			cell.state = Cell::CELL_EMPTY
-			expect(cell.to_s).to eq "░"
-			
-			cell.state = Cell::CELL_CROSSED
-			expect(cell.to_s).to eq "X"
 
-			cell.state = Cell::CELL_FULL
-			expect(cell.to_s).to eq "█"
-	end	
 end
