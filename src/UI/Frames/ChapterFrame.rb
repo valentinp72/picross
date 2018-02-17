2ce22772e989170e8159e3104dc99b71127fd77b
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2018-02-17 12:10:35 +0100

    * Added missing attribute reader

---
+++ src/UI/Frames/ChapterFrame.rb
@@ -0,0 +1,62 @@
+require 'yaml'
+require_relative 'GameFrame'
+require_relative 'OptionFrame'
+require_relative '../Frame'
+
+##
+# File          :: ChapterFrame.rb
+# Author        :: BROCHERIEUX Thibault
+# Licence       :: MIT License
+# Creation date :: 02/16/2018
+# Last update   :: 02/16/2018
+# Version       :: 0.1
+#
+# This class represents the ChapterFrame which list all chapter of an user
+class ChapterFrame < Frame
+
+	def initialize(user)
+		super()
+		self.border_width = 100
+
+		# Retrieve user's language
+		lang = user.settings.language
+		# Retrieve associated language config file
+		configFile = File.expand_path(File.dirname(__FILE__) + '/' + "../../../Config/lang_#{lang}")
+		config = YAML.load(File.open(configFile))
+
+
+
+		# Create 5 button
+		@playBtn = Gtk::Button.new(:label => config["home"]["play"])
+		@rankBtn = Gtk::Button.new(:label => config["home"]["rank"])
+		@ruleBtn = Gtk::Button.new(:label => config["home"]["rule"])
+		@optiBtn = Gtk::Button.new(:label => config["home"]["option"])
+		@exitBtn = Gtk::Button.new(:label => config["button"]["exit"])
+
+		# Create vertical box containing 5 boxes
+		@vbox = Gtk::Box.new(:vertical, 5)
+		@vbox.pack_start(@playBtn, :expand => true, :fill => true, :padding =>2)
+		@vbox.pack_start(@rankBtn, :expand => true, :fill => true, :padding =>2)
+		@vbox.pack_start(@ruleBtn, :expand => true, :fill => true, :padding =>2)
+		@vbox.pack_start(@optiBtn, :expand => true, :fill => true, :padding =>2)
+		@vbox.pack_start(@exitBtn, :expand => true, :fill => true, :padding =>2)
+
+		@playBtn.signal_connect("clicked") do
+			tmpMapPath = File.expand_path(File.dirname(__FILE__) + '/../../map.tmp/planet.map')
+			self.parent.setFrame(GameFrame.new(Map.load(tmpMapPath)))
+		end
+
+		# Redirecting user towards option menu
+		@optiBtn.signal_connect("clicked") do
+			self.parent.setFrame(OptionFrame.new(user))
+		end
+
+		# Exit programms
+		@exitBtn.signal_connect("clicked") do
+				Gtk.main_quit
+		end
+
+		# Add vbox to frame
+		add(@vbox)
+	end
+end
