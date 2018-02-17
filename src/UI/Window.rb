a95e20fcc13f55ae2eaef612f12e34df7415e3d6
Valentin Pelloin <valentinpelloin@gmail.com>
2018-02-17 15:10:18 +0100

    test logo gnome

--- src/UI/Window.rb
+++ src/UI/Window.rb
@@ -2,6 +2,8 @@ class Window < Gtk::ApplicationWindow
 
 	def initialize(application)
 		super(application)
+		testIcon = File.expand_path(File.dirname(__FILE__) + "../../../Documents/Logo/test_logo.png")
+		self.set_icon_from_file(testIcon)
 	end
 
 	def setFrame(frame)
