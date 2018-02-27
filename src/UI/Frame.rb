ae19ea81f7135dcce890ee9c6ea06f245c1bc807
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2018-02-27 13:33:33 +0100

    Now we user Picross class as our main object

--- src/UI/Frame.rb
+++ src/UI/Frame.rb
@@ -1,7 +1,10 @@
+require_relative '../Picross'
+
 class Frame < Gtk::Frame
 
 	def initialize()
 		super()
+		@picross = Picross.new()
 #self.resize_mode = Gtk::ResizeMode::QUEUE
 #self.resize_mode = Gtk::ResizeMode::QUEUE
 		self.shadow_type = Gtk::ShadowType::NONE
