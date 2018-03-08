42c02b6260f0dcddcf9a851e82adcfd2053e1fe6
Valentin Pelloin <valentinpelloin@gmail.com>
2018-03-08 11:01:48 +0100

    timer is here

--- src/Timer.rb
+++ src/Timer.rb
@@ -1,51 +1,88 @@
 ##
-# File		:: Timer.rb
-# Author	:: COHEN Mehdi
-# Licence	:: MIT Licence
-# Creation date	:: 01/27/2018
-# Last update	:: 01/30/2018
+# File          :: Timer.rb
+# Author        :: COHEN Mehdi, PELLOIN Valentin
+# Licence       :: MIT Licence
+# Creation date :: 01/27/2018
+# Last update   :: 03/08/2018
 #
-# This class represents a Timer
-# 
+# This class represents a Timer. A timer can be started, and then, it can be paused. When paused,
+# the timer can be started again (or unpaused). At any time, it's possible to get the total
+# elapsed seconds by the timer. It's also possible to get the elapsed time of the timer in a human
+# readable format (HH:MM::SS).
+
 class Timer
 
-	# +timer+	- basic timer
-
-	attr_reader :time
+	# Tells whether or not the timer is currently running
+	attr_reader :isRunning
 
 	##
-	# Create a new Timer object
-	# * *Arguments* :
-	#   - +time+		-> basic timer (hh/mm/ss)
-	def initialize(t=0)
-		@time = t
+	# Creation of a new Timer.
+	def initialize()
+		@startTimes = Array.new()
+		@endTimes   = Array.new()
+		@isRunning  = false
 	end
 
 	##
-    	# Method for launching a timer
+	# Tells the timer to start counting. This method
+	# does not overwrite the current elapsed time, it 
+	# add new times to it.
 	# * *Returns* :
-	#   - the time passed since the launching
-	def startTimer
-
-		while true
-			sleep 1
-			@time +=1
-          		hours = @time/3600
-    			min = (@time - (hours*3600)) / 60
-    			sec = @time - (hours*3600) - (min*60)
-
-    			h = hours < 10 ? "0" + hours.to_s : hours.to_s
-    			m = min < 10 ? "0" + min.to_s : min.to_s
-    			s = sec < 10 ? "0" + sec.to_s : sec.to_s
-          		system "clear"
-    			puts h + ":" + m + ":" + s
+	#   - the timer itself
+	def start()
+		if not self.isRunning then
+			@startTimes.push(Time.now)
+			@isRunning = true
 		end
-
+		return self
 	end
 
-end #Timer class
+	##
+	# Tells the timer to pause. To start again the timer, you 
+	# can use +start+ or +unpause+.
+	# * *Returns* :
+	#   - the timer itself
+	def pause()
+		if self.isRunning then
+			@endTimes.push(Time.now)
+			@isRunning = false
+		end
+		return self
+	end
 
-=begin
-timer = Timer.new(0)
-timer.startTimer()
-=end
+	alias unpause start
+
+	##
+	# Returns the total number of elapsed seconds since the timer began.
+	# It count the time of each run of the timer (pauses).
+	# * *Returns* :
+	#   - the elapsed seconds (Integer)
+	def elapsedSeconds()
+		time = 0
+		@startTimes.zip(@endTimes) do |startTime, endTime|
+			endTime = Time.now if endTime == nil
+			time += endTime - startTime
+		end
+		return time
+	end
+
+	##
+	# Return the total elapsed time by the timer, in a human-readable way.
+	# * *Returns* :
+	#   - the elapsed timer (String like "HH:MM:SS")
+	def elapsedTime()
+		return Time.at(self.elapsedSeconds).utc.strftime("%H:%M:%S")
+	end
+
+end
+
+timer = Timer.new()
+timer.start
+sleep 1
+timer.pause
+timer.unpause
+sleep 60
+timer.pause
+sleep 5
+#timer.pause
+puts timer.elapsedTime
