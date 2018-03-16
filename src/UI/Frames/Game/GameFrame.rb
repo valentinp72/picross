76a01634d069241b88e949259e7d0da55bf6b86d
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2018-03-16 13:38:31 +0100

    * begin to add popover menu

--- src/UI/Frames/Game/GameFrame.rb
+++ src/UI/Frames/Game/GameFrame.rb
@@ -7,6 +7,8 @@ require_relative '../OptionFrame'
 
 require_relative 'PicrossFrame'
 
+require_relative 'HypotheseFrame'
+
 class GameFrame < Frame
 
 	def initialize(user, chapter, map)
@@ -57,7 +59,6 @@ class GameFrame < Frame
 			hypotheses = @user.chapters[indexChapter].levels[indexMap].hypotheses
 			@map.hypotheses = hypotheses
 			@user.save()
-			print ("indexChapter : #{indexChapter}, indexMap : #{indexMap}")
 			self.parent.setFrame(MapFrame.new(@user,@chapter))
 		end
 
@@ -113,6 +114,10 @@ class GameFrame < Frame
 		@reset.signal_connect('clicked') do
 			@map.reset
 			@picross.grid = @map.hypotheses.getWorkingHypothesis.grid
+			@content.remove(@picross)
+			@content.pack_start(@picross, :expand => true, :fill => true)
+			@content.reorder_child(@picross,0)
+
 		end
 
 		@labelPause =  Gtk::Label.new("Partie en pause")
@@ -146,16 +151,38 @@ class GameFrame < Frame
 			end
 		end
 
+
+
+		@popover = Gtk::Popover.new()
+		@label1 =  Gtk::Label.new("Test")
+		@label2 =  Gtk::Label.new("Test2")
+		@label3 =  Gtk::Label.new("Test3")
+
+		@labelbox = Gtk::Box.new(:horizontal)
+
+		@labelbox.pack_start(@label1, :expand => true, :fill => true)
+		@labelbox.pack_start(@label2, :expand => true, :fill => true)
+		@labelbox.pack_start(@label3, :expand => true, :fill => true)
+
+		@popover.position = :top
+		@popover.add(@labelbox)
+		@labelbox.show_all
+
+
+
 		@btnHypotheses = Gtk::Button.new()
 		@btnHypotheses.image  = AssetsLoader.loadImage('light-bulb.png', 40)
 		@btnHypotheses.relief = Gtk::ReliefStyle::NONE
 		@btnHypotheses.signal_connect('clicked') do
+
+			puts "show sow"
+			@popover.visible = true
+
+
 			@map.hypotheses.addNewHypothesis
 			@grid = @map.hypotheses.getWorkingHypothesis.grid
 			@picross.grid = @grid
 		end
-
-
 
 		@help  = Gtk::Button.new()
 		@help.image = AssetsLoader.loadImage("help.jpg",40,40)
