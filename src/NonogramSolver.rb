4e78edd5ab985779262b14130347e47a5258657f
Shirenator <v.debonne@neuf.fr>
2018-03-29 14:57:46 +0200

    Update NonogramSolver.rb

--- src/NonogramSolver.rb
+++ src/NonogramSolver.rb
@@ -225,6 +225,7 @@ class NonogramSolver
 	end
 end
 
+#Several picross grids
 p1 = ["C BA CB BB F AE F A B", "AB CA AE GA E C D C"]
 p2 = ["F CAC ACAC CN AAA AABB EBB EAA ECCC HCCC", "D D AE CD AE A DA BBB CC AAB BAA AAB DA AAB AAA BAB AAA CD BBA DA"]
 p3 = ["CA BDA ACC BD CCAC CBBAC BBBBB BAABAA ABAD AABB BBH BBBD ABBAAA CCEA AACAAB BCACC ACBH DCH ADBE ADBB DBE ECE DAA DB CC", "BC CAC CBAB BDD CDBDE BEBDF ADCDFA DCCFB DBCFC ABDBA BBF AAF BADB DBF AAAAD BDG CEF CBDB BBB FC"]
@@ -251,11 +252,12 @@ def convert (lins,cols)
 	return [resLins[0,resLins.size-1],resCols[0,resCols.size-1]]
 end
 
+#Several other picross grids
 #Me
 lines = [[2,3,2],[1,4,4,1],[6,6],[15],[3,3,1,3],[3,1,1,5],[3,1,1,1,4],[3,3,1,5],[2,3,1,2],[1,11,1],[2,9,2],[3,7,3],[4,5,4],[5,3,5],[6,1,6]]
 clns = [[2,5,6],[1,7,5],[9,4],[3,2,3],[4,6,2],[5,6,1],[1,3,8],[1,1,6],[1,12],[3,4,1],[3,1,1,3,2],[3,3,2,3],[9,4],[1,7,5],[2,5,6]]
 
-#Piou (fonctionne pas)
+#Piou
 #lines = [[1],[3,2],[2,2,1,1],[2,2,4],[1,2,1,2],[2,2,2],[1,3],[2],[2,2],[1,1]]
 #clns = [[1,2,1],[3,2,2],[1,2,1],[3,2,2],[2,2,1],[2,1,1],[2],[4],[1,2],[3],[1]]
 
@@ -267,7 +269,7 @@ clns = [[2,5,6],[1,7,5],[9,4],[3,2,3],[4,6,2],[5,6,1],[1,3,8],[1,1,6],[1,12],[3,
 #lines = [[2],[3],[2,1],[2,1],[3,1],[4,1,1],[1,1,1],[1,1,1],[1,1,1],[4,1,1],[3,1],[2,1],[2,1],[3],[2]]
 #clns = [[1,1],[1,1],[1,1],[7],[1,1],[9],[2,2],[2,2],[2,2],[15]]
 
-
+#Main to test methods
 [convert(lines,clns)].each do |p|
 	solver = NonogramSolver.new(p)
 	solver.solve
