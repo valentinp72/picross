606cf5773a12a001e0f45ee21eaf55a5f26a8d71
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2018-03-31 12:07:20 +0200

    * fixed

--- src/UI/Frames/StatsFrame.rb
+++ src/UI/Frames/StatsFrame.rb
@@ -21,94 +21,22 @@ class StatsFrame < Frame
 		@lang = user.lang
 
 		# Create 3 button
-		@playerBtn = Gtk::Button.new(:label => @lang["stats"]["player"])
-		@globalBtn = Gtk::Button.new(:label => @lang["stats"]["globals"])
-
-		@returnBtn = Gtk::Button.new()
-		@returnBtn.image = AssetsLoader.loadImage("arrow-left.png", 40)
-		@returnBtn.relief = Gtk::ReliefStyle::NONE
-
-		@hbox = Gtk::Box.new(:horizontal)
-		@hbox.pack_start(@playerBtn, :expand => true, :fill => true, :padding =>2)
-		@hbox.pack_start(@globalBtn, :expand => true, :fill => true, :padding =>2)
+		@playerBtn = createPlayerButton(user)
+		@globalBtn = createGlobalButton(user)
 
 		@scrolled = Gtk::ScrolledWindow.new
 		@scrolled.set_policy(:never, :automatic)
 
-		@vbox = Gtk::Box.new(:vertical)
-		@vbox.pack_start(@returnBtn, :expand => false, :fill => true, :padding =>2)
-		@vbox.pack_start(@hbox, :expand => false, :fill => true, :padding =>2)
-		@vbox.pack_start(@scrolled, :expand => true, :fill => true, :padding =>2)
-
 		@listbox = Gtk::ListBox.new
 		@scrolled.add(@listbox)
+
+		@container = createMainLayout(user)
 
 		@playerHeader = Gtk::Box.new(:horizontal)
 		@playerHeader.set_homogeneous(true)
 
-		@playerBtn.signal_connect("clicked") do
-			i = 1
-			reset(@listbox)
-			createPlayerScrollHeader()
-
-			# Loop over each Map of the user
-			user.chapters.each do |chap|
-				chap.levels.each do |lvl|
-					if(lvl.allStat.length != 0)
-						createPlayerScrollData(lvl)
-						i += 1
-					end
-				end
-			end
-
-			# If i = 1 it means that there is no stat recorded for that user
-			if(i == 1) then
-				box = Gtk::Box.new(:horizontal)
-				box.set_homogeneous(true)
-				box.pack_startattach(Gtk::Label.new(@lang["stats"]["empty"]),:expand => true, :fill => true, :padding =>2)
-			end
-			@listbox.show_all
-		end
-
-		# Redirecting user towards option menu
-		@globalBtn.signal_connect("clicked") do
-			reset(@listbox)
-			removePlayerScrollHeader()
-
-			totalClick = 0
-			totalTime = 0
-			totalHelp = 0
-			totalStar = 0
-			totalFinished = 0
-
-			# Loop over each Map of the user
-			user.chapters.each do |chap|
-				chap.levels.each do |lvl|
-					lvl.allStat.each do |stat|
-						totalClick += stat.nbClick
-						totalTime += stat.time.elapsedSeconds
-						totalHelp += stat.usedHelp
-						totalStar += stat.numberOfStars
-						totalFinished += 1 if stat.isFinished == true
-					end
-				end
-			end
-
-			createGlobalScroll(@lang["stats"]["global"]["stars"], totalStar.to_s)
-			createGlobalScroll(@lang["stats"]["global"]["time"], totalTime.to_s)
-			createGlobalScroll(@lang["stats"]["global"]["help"], totalHelp.to_s)
-			createGlobalScroll(@lang["stats"]["global"]["click"], totalClick.to_s)
-			createGlobalScroll(@lang["stats"]["global"]["finished"],totalFinished.to_s)
-			@listbox.show_all
-		end
-
-		# Redirecting user towards home
-		@returnBtn.signal_connect("clicked") do
-			self.parent.setFrame(HomeFrame.new(user))
-		end
-
 		# Add vbox to frame
-		add(@vbox)
+		add(@container)
 	end
 
 	##
@@ -138,13 +66,13 @@ class StatsFrame < Frame
 		@playerHeader.pack_start(Gtk::Label.new(@lang["stats"]["local"]["click"]),:expand => true, :fill => true, :padding =>2)
 		@playerHeader.pack_start(Gtk::Label.new(@lang["stats"]["local"]["finished"]),:expand => true, :fill => true, :padding =>2)
 		@playerHeader.show_all
-		@vbox.pack_start(@playerHeader, :expand => false, :fill => true, :padding =>2)
-		@vbox.reorder_child(@playerHeader,2)
+		@container.pack_start(@playerHeader, :expand => false, :fill => true, :padding =>2)
+		@container.reorder_child(@playerHeader,2)
 	end
 
 	def removePlayerScrollHeader()
-		if(@playerHeader.parent == @vbox) then
-			@vbox.remove(@playerHeader)
+		if(@playerHeader.parent == @container) then
+			@container.remove(@playerHeader)
 		end
 	end
 
@@ -160,4 +88,99 @@ class StatsFrame < Frame
 		@listbox.add(box)
 	end
 
+	def calculatePlayerStat(user)
+		i = 1
+		# Loop over each Map of the user
+		user.chapters.each do |chap|
+			chap.levels.each do |lvl|
+				if(lvl.allStat.length != 0)
+					createPlayerScrollData(lvl)
+					i += 1
+				end
+			end
+		end
+		return i
+	end
+
+	def createPlayerButton(user)
+		button = Gtk::Button.new(:label => @lang["stats"]["player"])
+		button.signal_connect("clicked") do
+			reset(@listbox)
+			createPlayerScrollHeader()
+
+			i = calculatePlayerStat(user)
+
+			# If i = 1 it means that there is no stat recorded for that user
+			if(i == 1) then
+				box = Gtk::Box.new(:horizontal)
+				box.set_homogeneous(true)
+				box.pack_start(Gtk::Label.new(@lang["stats"]["empty"]),:expand => true, :fill => true, :padding =>2)
+			end
+			@listbox.show_all
+		end
+		return button
+	end
+
+	def calculateGlobalStat(user)
+		totalClick = 0
+		totalTime = 0
+		totalHelp = 0
+		totalStar = 0
+		totalFinished = 0
+
+		# Loop over each Map of the user
+		user.chapters.each do |chap|
+			chap.levels.each do |lvl|
+				lvl.allStat.each do |stat|
+					totalClick += stat.nbClick
+					totalTime += stat.time.elapsedSeconds
+					totalHelp += stat.usedHelp
+					totalStar += stat.numberOfStars
+					totalFinished += 1 if stat.isFinished == true
+				end
+			end
+		end
+		return [totalClick, totalTime, totalHelp ,totalStar, totalFinished]
+	end
+
+	def createGlobalButton(user)
+		button = Gtk::Button.new(:label => @lang["stats"]["globals"])
+
+		# Redirecting user towards option menu
+		button.signal_connect("clicked") do
+			reset(@listbox)
+			removePlayerScrollHeader()
+
+			totalClick, totalTime, totalHelp ,totalStar, totalFinished = calculateGlobalStat(user)
+
+			createGlobalScroll(@lang["stats"]["global"]["stars"], totalStar.to_s)
+			createGlobalScroll(@lang["stats"]["global"]["time"], totalTime.to_s)
+			createGlobalScroll(@lang["stats"]["global"]["help"], totalHelp.to_s)
+			createGlobalScroll(@lang["stats"]["global"]["click"], totalClick.to_s)
+			createGlobalScroll(@lang["stats"]["global"]["finished"],totalFinished.to_s)
+			@listbox.show_all
+		end
+		return button
+	end
+
+	def createMainLayout(user)
+
+		@hbox = Gtk::Box.new(:horizontal)
+		@hbox.pack_start(@playerBtn, :expand => true, :fill => true, :padding =>2)
+		@hbox.pack_start(@globalBtn, :expand => true, :fill => true, :padding =>2)
+
+		@returnBtn = Gtk::Button.new()
+		@returnBtn.image = AssetsLoader.loadImage("arrow-left.png", 40)
+		@returnBtn.relief = Gtk::ReliefStyle::NONE
+
+		# Redirecting user towards home
+		@returnBtn.signal_connect("clicked") do
+			self.parent.setFrame(HomeFrame.new(user))
+		end
+
+		box = Gtk::Box.new(:vertical)
+		box.pack_start(@returnBtn, :expand => false, :fill => true, :padding =>2)
+		box.pack_start(@hbox, :expand => false, :fill => true, :padding =>2)
+		box.pack_start(@scrolled, :expand => true, :fill => true, :padding =>2)
+	end
 end
