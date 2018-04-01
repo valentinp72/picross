09db73e5f4799a405f054a10f88b1ea0db6bbfe4
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2018-04-02 01:07:28 +0200

    * Added UT

--- spec/User_spec.rb
+++ spec/User_spec.rb
@@ -21,10 +21,6 @@ describe User do
 		expect(User.languagesName).to eq ['english', 'francais']
 	end
 
-	# it "change to default language" do
-	# 	expect(User.defaultLang()).to eq '{"login"=>{"login"=>"Login", "new"=>"Create new account"}, "newUser"=>{"username"=>"Enter username:",...me"=>"Total time", "help"=>"Total used help", "click"=>"Total click", "finished"=>"Finished game"}}}'
-	# end
-
 	it "check totalStars" do
 		expect(user.totalStars).to eq 0
 	end
@@ -44,4 +40,10 @@ describe User do
 	it "add -10 help to user(impossible to add negative)" do
 		expect{user.addHelp(-10)}.to raise_error(User::NegativeAmountException)
 	end
+
+	it "test marshal load/dump" do
+		temp = Marshal.dump(user)
+		temp2 = Marshal.load(temp)
+	end
+
 end
