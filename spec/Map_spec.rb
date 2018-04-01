0360ed5cdbae022236711f3abe96b9c3734bbf0d
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2018-04-01 17:02:13 +0200

    * Begin unitary test for Map

---
+++ spec/Map_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Map do
+
+	path = File.expand_path(File.dirname(__FILE__) + '/' + '../Chapter/Chapter1')
+	map = nil
+
+	# it "load a map" do
+	# 	puts path + "/batman.map"
+	# 	map = Map.load(path + '/batman.map')
+	# end
+	#
+	# it "save a map" do
+	# 	map.save("batman")
+	# end
+
+end
