47117752841169a34c217551e77841d7109c8e48
Valentin Pelloin <valentinpelloin@gmail.com>
2018-04-04 23:41:57 +0200

    comments chapterframe

--- src/UI/Frames/ChapterFrame.rb
+++ src/UI/Frames/ChapterFrame.rb
@@ -2,6 +2,7 @@ require 'yaml'
 
 require_relative 'MapFrame'
 require_relative 'HomeFrame'
+
 require_relative '../Frame'
 require_relative '../AssetsLoader'
 require_relative '../GridCreator'
@@ -9,19 +10,26 @@ require_relative '../ButtonCreator'
 
 ##
 # File          :: ChapterFrame.rb
-# Author        :: BROCHERIEUX Thibault
+# Author        :: BROCHERIEUX Thibault, PELLOIN Valentin
 # Licence       :: MIT License
 # Creation date :: 02/16/2018
-# Last update   :: 02/16/2018
+# Last update   :: 04/04/2018
 # Version       :: 0.1
 #
 # This class represents the ChapterFrame which list all chapter of an user
+
 class ChapterFrame < Frame
 
+	# The image to show when a chapter is locked
 	LOCK_IMAGE = AssetsLoader.loadImage('lock.png', 20)
 
+	# The image to explain a star
 	STAR_IMAGE = AssetsLoader.loadImage('star.png', 20)
 
+	##
+	# Create a new ChapterFrame, displaying all the chapters of a User.
+	# * *Arguments* :
+	#   - +user+ -> the user to show it's chapters
 	def initialize(user)
 		super()
 		@user = user
@@ -29,6 +37,10 @@ class ChapterFrame < Frame
 		self.add(createArea)
 	end
 
+	##
+	# Create the main area for this frame (a Gtk::Grid).
+	# * *Returns* :
+	#   - a Gtk::Grid with everything inside
 	def createArea
 		buttons = []
 		buttons << self.createReturnButton
@@ -38,6 +50,10 @@ class ChapterFrame < Frame
 		return GridCreator.fromArray(buttons, :vertical => true)
 	end
 
+	##
+	# Create the return button
+	# * *Returns* :
+	#   - the return button to the main page
 	def createReturnButton
 		return ButtonCreator.new(
 			:assetName => "arrow-left.png",
@@ -47,6 +63,12 @@ class ChapterFrame < Frame
 		)
 	end
 
+	##
+	# Create a button for a chapter
+	# * *Arguments* :
+	#   - +chapter+ -> the chapter to show the button about
+	# * *Returns* :
+	#   - a clickable Gtk::Button for the given chapter
 	def createChapterButton(chapter)
 		contents = self.chapterButtonContents(chapter)
 		content = GridCreator.fromArray(contents, :horizontal => true, :xSizes => [10])
@@ -59,6 +81,13 @@ class ChapterFrame < Frame
 		return button
 	end
 
+	##
+	# Create and return an Array with every Gtk::Widget that sould 
+	# be put inside the button of a chapter.
+	# * *Arguments* :
+	#   - +chapter+ -> the chapter to show the content about
+	# * *Returns* :
+	#   - an array with plenty of Gtk::Widget 
 	def chapterButtonContents(chapter)
 		contents = []
 
@@ -73,8 +102,13 @@ class ChapterFrame < Frame
 		return contents
 	end
 
+	##
+	# The action to do when the return button is clicked
+	# * *Returns* :
+	#   - the frame itself
 	def btn_return_clicked
 		self.parent.setFrame(HomeFrame.new(@user))
+		return self
 	end
 
 end
