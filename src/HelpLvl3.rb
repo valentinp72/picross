0c2a349fe80458b976181d128f0695a893b72743
Shirenator <v.debonne@neuf.fr>
2018-04-07 12:28:13 +0200

    ajout des aides oubliées hier

---
+++ src/HelpLvl3.rb
@@ -0,0 +1,36 @@
+require_relative 'Map'
+require_relative 'User'
+require_relative 'Help'
+require_relative 'Helper'
+
+##
+# File          :: HelpLvl3.rb
+# Author        :: Debonne Valentin
+# Licence       :: MIT License
+# Creation date :: 04/01/2018
+# Last update   :: 04/07/2018
+# Version       :: 0.1
+#
+# This class represents a hard help
+class HelpMadeError < Help
+
+	# The cost of this help (in seconds)
+	COST_HELP = 30
+
+	def initialize(map, user)
+		super(map, user, COST_HELP)
+		@helper = Helper.new(@map)
+	end
+
+	def apply
+		super()	
+		tabBox = @helper.traitement(@map, 3)
+		tabBox.each do |box|
+			y = box[0]
+			x = box[1]
+			stat = box[2]
+			@map.grid.cellPosition(y, x).state=(stat)
+		end
+	end
+
+end
