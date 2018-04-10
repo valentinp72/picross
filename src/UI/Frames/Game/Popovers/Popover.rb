1d517534cadd96056de03e82301e9701836da71c
Valentin Pelloin <valentinpelloin@gmail.com>
2018-04-10 17:58:25 +0200

    comments popover

--- src/UI/Frames/Game/Popovers/Popover.rb
+++ src/UI/Frames/Game/Popovers/Popover.rb
@@ -1,15 +1,36 @@
+##
+# File          :: Popover.rb
+# Author        :: PELLOIN Valentin
+# Licence       :: MIT License
+# Creation date :: 05/04/2018
+# Last update   :: 10/04/2018
+# Version       :: 0.1
+#
+# This abstract class represents a povover in the application. 
+
 class Popover < Gtk::Popover
 	
+	##
+	# Create a new popover on a button
+	# * *Arguments* :
+	#   - +button+ -> the button the popover is linked on
 	def initialize(button)
 		super(button)
 		
 		self.position = :top
 	end
 
+	##
+	# Abstract method to get the content (must be a Gtk::Widget)
+	# of the Popover.
 	def content
 		raise NotImplementedError, "a Popover needs to know what to do when getting it's content"
 	end
 
+	##
+	# Set the content of the popover
+	# * *Returns* :
+	#   - the object itself
 	def setContent()
 		self.children.each do |child|
 			self.remove(child)
@@ -17,10 +38,16 @@ class Popover < Gtk::Popover
 		toAdd = self.content
 		toAdd.show_all
 		self.add(toAdd)
+		return self
 	end
 
+	##
+	# Update the content of the popover
+	# * *Returns* :
+	#   - the object itself
 	def update
 		self.setContent
+		return self
 	end
 
 end
--- src/UI/Frames/Game/Popovers/PopoverHelps.rb
+++ src/UI/Frames/Game/Popovers/PopoverHelps.rb
@@ -8,18 +8,41 @@ require_relative '../../../../HelpLvl3'
 
 require_relative 'Popover'
 
+##
+# File          :: PopoverHelps.rb
+# Author        :: PELLOIN Valentin
+# Licence       :: MIT License
+# Creation date :: 05/04/2018
+# Last update   :: 10/04/2018
+# Version       :: 0.1
+#
+# This class represents a Povover allowing the user to use Helps. 
+
 class PopoverHelps < Popover
 
+	##
+	# Create a new popover for helps
+	# * *Arguments* :
+	#   - +button+ -> the button the popover is linked on
+	#   - +frame+  -> the frame of the popover
 	def initialize(button, frame)
 		super(button)
 		@frame   = frame
 		@helpers = self.helpers
 	end
 
+	##
+	# Return a Gtk::Grid with all the wigets of this popover
+	# * *Returns* : 
+	#   - a Gtk::Grid
 	def content
 		return GridCreator.fromArray(self.contents, :vertical => true)
 	end
 
+	##
+	# Gets the content to be put inside the popver (an array)
+	# * *Returns* :
+	#   - an Array of Gtk::Widget
 	def contents
 		contents = []
 
@@ -31,6 +54,10 @@ class PopoverHelps < Popover
 		return contents
 	end
 
+	##
+	# Returns the helps modules to be put inside the povover (an Array)
+	# * *Returns* :
+	#   - an Array of Help
 	def helpers
 		helpers = []
 
@@ -42,6 +69,10 @@ class PopoverHelps < Popover
 		return helpers
 	end
 
+	##
+	# Create the title of the popover
+	# * *Returns* : 
+	#   - a Gtk::Label
 	def createTitle
 		label = Gtk::Label.new
 		title = @frame.user.lang['help']['available'].gsub('{{HELPS}}', @frame.user.availableHelps.to_s)
@@ -49,6 +80,13 @@ class PopoverHelps < Popover
 		return label
 	end
 
+	##
+	# Create the area (a Gtk::Grid) with all the content
+	# of the helper inside
+	# * *Arguments* :
+	#   - +helper+ -> an Help
+	# * *Returns* :
+	#   - a Gtk::Grid
 	def createHelperArea(helper)
 		lang     = @frame.user.lang
 		helpInfo = lang['helps'][helper.class.name]
@@ -58,8 +96,8 @@ class PopoverHelps < Popover
 
 		description = Gtk::Label.new(helpInfo['description'])
 
-		costP = createCostLabel('costPenalty', helper.costSeconds)
-		costH = createCostLabel('costHelp',    helper.costHelps)
+		costP = createCostLabel('costPenalty', helper)
+		costH = createCostLabel('costHelp',    helper)
 
 		if @frame.user.availableHelps >= helper.costHelps then
 			useHelp = lang['help']['costHelp'].gsub('{{COST}}', (helper.costHelps).to_s)
@@ -73,29 +111,50 @@ class PopoverHelps < Popover
 		return GridCreator.fromArray(helperContent, :vertical => true)
 	end
 
-	def createCostLabel(costName, value)
+	##
+	# Create the Gtk::Label with the cost of the help
+	# * *Arguments* :
+	#   - +costName+ -> the name of the cost (in the language config file)
+	#   - +helper+   -> the Help of the label
+	# * *Returns* :
+	#   - a Gtk::Label, with sometimes a strikethrough if it's not the appropriate help
+	def createCostLabel(costName, helper)
 		cost = Gtk::Label.new
 		lang = @frame.user.lang['help']
-		name = lang['cost'] + lang[costName].gsub('{{COST}}', value.to_s)
 
-		if costName == 'costHelp' && @frame.user.availableHelps < value  ||
-			 costName == 'costPenalty' && @frame.user.availableHelps >= (value / 30)
+		if costName == 'costHelp' then
+			value = helper.costHelps
+		elsif costName == 'costPenalty' then
+			value = helper.costPenalty
+		end
+
+		if costName == 'costHelp' && @frame.user.availableHelps < helper.costSeconds  ||
+			 costName == 'costPenalty' && @frame.user.availableHelps >= helper.costHelps
 			then
 			before = '<span strikethrough="true">'
 			after  = '</span>'
 		end
 		before ||= ''
 		after  ||= ''
+		
+		name = lang['cost'] + lang[costName].gsub('{{COST}}', value.to_s)
 
 		cost.markup = before + name + after
 		return cost
 	end
 
+	##
+	# Action to be done when clicking on the help
+	# * *Arguments* :
+	#   - +help+ -> the Help
+	# * *Returns* :
+	#   - the object itself
 	def btn_useHelp_clicked(help)
 		help.apply
 		@frame.checkMap
 		@frame.updateGrid
 		self.hide
+		return self
 	end
 
 end
